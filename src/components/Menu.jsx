a78783e4e072acb16dfcf74627d696596d440dc1
Osher <osherasfa@gmail.com>
2023-07-09 22:01:04 +0300

    Updates v1.0.8

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 // import dinoChill from "./assets/dino_chill.mp3"
 
+// UNDER DEVELOPMENT
 export default function Menu({children}){
   // const audio = new Audio(dinoChill)
-  const [ showMenu, setShowMenu ] = React.useState(true)
+  const [ showMenu, setShowMenu ] = React.useState(false)
   return (
     <div className="menu">
       {showMenu ? <h1 onClick={() => setShowMenu(false)}>Menu</h1> : React.Children.map(children, child => React.cloneElement(child, { isHistory: true, isLimited: false }))}
