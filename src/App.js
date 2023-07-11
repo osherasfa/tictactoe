32f4601f549a27181b4c4efb5681534256afb839
Osher <osherasfa@gmail.com>
2023-07-11 20:39:42 +0300

    Updates v1.1.0: Added menu, BG music ON/OFF, custom theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import React from "react";
+import Game from "./components/Game";
 import Menu from "./components/Menu";
 import Logo from "./assets/images/Logo";
+import Click from "./assets/soundEffects/clickTwo.wav"
+
+const btnClick = new Audio(Click)
 
 function App() {
+  const [ showMenu, setShowMenu ] = React.useState(true)
+  const [ picker, setPicker ] = React.useState(false)
+  const [ theme, setTheme ] = React.useState({ background: "#000000", color: "#00ff00"})
+  const [ music, setMusic ] = React.useState(false)
+
+  document.documentElement.style.setProperty('--color', theme.color)
+  document.documentElement.style.setProperty('--background', theme.background)
+
+  const btnEls = document.getElementsByClassName("button")
+  for (let i = 0; i < btnEls.length; i++)
+    btnEls.item(i).addEventListener("click", () => btnClick.play())
+
   return (
     <div className="App">
         <Logo/>
-        <Menu/>
+        {showMenu 
+          ? <Menu  
+              theme={theme}
+              picker={picker}
+              music={music}
+              openTheme={() => setPicker(prevPicker => !prevPicker)}
+              changeTheme={(event) => setTheme(prevTheme => ({...prevTheme, [event.target.id]: event.target.value}))}
+              activeMusic={() => setMusic(prevMusic => !prevMusic)}
+              start={() => setShowMenu(false)}
+            /> 
+          : <Game isHistory={true} isLimited={false}/>}
     </div>
   );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,72 +1,34 @@
 import React from "react";
-import Game from "./Game"
-// import dinoChill from "./assets/dino_chill.mp3"
+import GameAudio from "../assets/images/GameAudio"
+import dinoChill from "../assets/soundEffects/dino_chill.mp3"
 
-function hexToHue(hexColor) {
-  hexColor = hexColor.replace('#', '');
+const dinoAudio = new Audio(dinoChill)
+dinoAudio.loop = true
 
-  // Convert hexadecimal to RGB values
-  const r = parseInt(hexColor.substring(0, 2), 16);
-  const g = parseInt(hexColor.substring(2, 4), 16);
-  const b = parseInt(hexColor.substring(4, 6), 16);
-
-  // Normalize RGB values to range [0, 1]
-  const normalizedR = r / 255;
-  const normalizedG = g / 255;
-  const normalizedB = b / 255;
-
-  // Find the maximum and minimum RGB values
-  const max = Math.max(normalizedR, normalizedG, normalizedB);
-  const min = Math.min(normalizedR, normalizedG, normalizedB);
-
-  // Calculate the hue
-  let hue;
-  if (max === min)
-    hue = 0; // achromatic (gray)
-  else {
-    var delta = max - min;
-    // eslint-disable-next-line
-    switch (max) {
-      case normalizedR:
-        hue = (normalizedG - normalizedB) / delta + (normalizedG < normalizedB ? 6 : 0);
-        break;
-      case normalizedG:
-        hue = (normalizedB - normalizedR) / delta + 2;
-        break;
-      case normalizedB:
-        hue = (normalizedR - normalizedG) / delta + 4;
-        break;
-    }
-    hue /= 6;
-  }
-
-  // Convert hue to degrees (0-360)
-  hue *= 360;
-
-  // Round hue value to two decimal places
-  hue = Math.round(hue * 100) / 100;
-
-  return hue;
-}
-
-
-const theme = { background: "black", color: "#00ff00"}
-
-const hue = hexToHue(theme.color)
-theme["secondary"] = `hsl(${hue}, 100%, 30%)`
-theme["tertiary"] = `hsl(${hue}, 100%, 80%)`
-
-export default function Menu(){
-  const [ showMenu, setShowMenu ] = React.useState(true)
-  // const audio = new Audio(dinoChill)
-  document.documentElement.style.setProperty('--color', theme.color)
-  document.documentElement.style.setProperty('--background', theme.background)
-  document.documentElement.style.setProperty('--color-secondary', theme.secondary)
-  document.documentElement.style.setProperty('--color-tertiary', theme.tertiary)
+export default function Menu({ start, theme, picker, music, openTheme, changeTheme, activeMusic }){
+  const isDottedTheme = theme.background.includes("url")
+  music ? dinoAudio.play() : dinoAudio.pause()
 
   return (
     <div className="menu">
-      {showMenu ? <h1 className="button" onClick={() => setShowMenu(false)}>Menu</h1> : <Game isHistory={true} isLimited={false}/>}
+      <h1 onClick={start} className="button">Play</h1>
+      <h1 onClick={openTheme} className="button" style={picker ? {marginBottom: "0"} : null }>Theme</h1>
+
+      { picker ? (
+            <div>
+              <h5 className={`button${isDottedTheme ? "" : " disabled"}`}><span style={{fontSize: "8px"}}>(under development)</span><br/>dotted</h5>
+              <h5 className={`button${isDottedTheme ? " disabled" : ""}`}>8-bit</h5>
+
+              <div id="picker-container">
+                <label>color: <input type="color" id="color" className="picker" onChange={changeTheme} value={theme.color}/> &nbsp;</label>
+                <label>background: <input type="color" id="background" className="picker" onChange={changeTheme}  value={theme.background}/></label>
+              </div>
+
+
+            </div>
+      ) : null }
+
+      <h1 className="button"> <GameAudio onClick={activeMusic}  music={music}/> </h1>
     </div>
   );
 }
