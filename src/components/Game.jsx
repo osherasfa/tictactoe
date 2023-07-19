cfca6e3d6bdd2b2dfd354277b0f8094a14eb0a59
Osher <osherasfa@gmail.com>
2023-07-19 13:48:05 +0300

    Updates v1.1.3: Under development

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,5 @@
 import "../styles/Game.css";
-import React, { useState, useEffect, useRef } from "react"
+import React, { useState, useEffect, useRef, useMemo } from "react"
 import Board from "./Board"
 import History from "./History"
 
@@ -73,6 +73,101 @@ export default function Game({settings}){
     }
   }
 
+  function isEqual(testItem, item){
+    return !!testItem && testItem === item
+  }
+
+  function checkWinner(){
+    let isRow = true, isColumn = true
+    let isDiagnolRight = true, isDiagnolLeft = true
+    
+    for(let i = 0; i < board.length; i += 4){
+      isRow = true
+      isColumn = true
+
+      for(let j = i; j < i + 4; j++){
+        if(isRow && !isEqual(board[i], board[j]))
+          isRow = false
+        if(isColumn && !isEqual(board[i / 4], board[(j-i)*4 + i / 4]))
+          isColumn = false
+        if(isDiagnolRight && !(j % 5) && !isEqual(board[0], board[j]))
+          isDiagnolRight = false
+        if(isDiagnolLeft && (j !== 0 && j !== 15) && !(j % 3) && !isEqual(board[3], board[j]))
+          isDiagnolLeft = false
+      }
+
+      if(isRow || isColumn)
+        break;
+    }
+
+    return isRow || isColumn || isDiagnolRight || isDiagnolLeft
+  }
+
+  useMemo(() => {
+    function bestMove(){
+        const newBoard = [...board]
+        let bestScore = -Infinity
+        let move;
+
+        for(let i = 0; i < board.length; i += 4){
+          for(let j = i; j < i + 4; j++){
+            if(!newBoard[j] && !currentPlayer){
+              newBoard[j] = "O"
+              let score = minimax(newBoard, 0, false)
+              newBoard[j] = null
+
+              if(score > bestScore){
+                bestScore = score
+                move = j
+              }
+          }
+        }
+      }
+      return move
+    }
+
+    function minimax(currentBoard, depth, isMaximizing){
+      if(checkWinner())
+        return currentPlayer ? -1 : !currentBoard.includes(null) ? 0 : 1
+      
+      if(isMaximizing){
+        let bestScore = -Infinity
+        for(let i = 0; i < currentBoard.length; i += 4){
+          for(let j = i; j < i + 4; j++){
+            if(!currentBoard[j] && !currentPlayer){
+              currentBoard[j] = "O"
+              let score = minimax(currentBoard, depth + 1, false)
+              currentBoard[j] = null
+              bestScore = Math.max(score, bestScore)
+            }
+          }
+        }
+        return bestScore
+      }
+      else{
+        let bestScore = Infinity
+        for(let i = 0; i < currentBoard.length; i += 4){
+          for(let j = i; j < i + 4; j++){
+            if(!currentBoard[j] && !currentPlayer){
+              currentBoard[j] = "X"
+              let score = minimax(currentBoard, depth + 1, true)
+              currentBoard[j] = null
+              bestScore = Math.min(score, bestScore)
+            }
+          }
+        }
+        return bestScore
+      }
+    }
+
+    if(!currentPlayer){
+      drawPlayer(bestMove())
+    }
+    // eslint-disable-next-line
+  }, [currentPlayer])
+
+
+
   useEffect(() => {
     function changeGameStatus(status){
       if(status === ON_GOING)
@@ -95,35 +190,8 @@ export default function Game({settings}){
       }
     }
 
-    function isEqual(testItem, item){
-      return !!testItem && testItem === item
-    }
-
     function isWon(){
-      let isRow = true, isColumn = true
-      let isDiagnolRight = true, isDiagnolLeft = true
-      
-      for(let i = 0; i < board.length; i += 4){
-        isRow = true
-        isColumn = true
-
-        for(let j = i; j < i + 4; j++){
-          if(isRow && !isEqual(board[i], board[j]))
-            isRow = false
-          if(isColumn && !isEqual(board[i / 4], board[(j-i)*4 + i / 4]))
-            isColumn = false
-          if(isDiagnolRight && !(j % 5) && !isEqual(board[0], board[j]))
-            isDiagnolRight = false
-          if(isDiagnolLeft && (j !== 0 && j !== 15) && !(j % 3) && !isEqual(board[3], board[j]))
-            isDiagnolLeft = false
-        }
-
-        if(isRow || isColumn)
-          break;
-      }
-
-
-      if(isRow || isColumn || isDiagnolRight || isDiagnolLeft)
+      if(checkWinner())
         changeGameStatus(WIN)
       else if(!board.includes(null))
         changeGameStatus(DRAW)
@@ -135,8 +203,8 @@ export default function Game({settings}){
       disableWinChecker.current = false
     }
     else
-    isWon()
-
+      isWon()
+    // eslint-disable-next-line
   },[board, settings])
 
   const status =  gameStatus === WIN ? `${symbol} is the winner!` :
